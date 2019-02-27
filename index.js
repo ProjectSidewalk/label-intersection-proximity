a77b327f3e3faea5d4a959aabe716a878579a4fa
Anthony Li <antli.inbox@gmail.com>
2019-02-27 16:14:19 -0500

    Removed unused file

--- index.js
+++
@@ -1,7 +0,0 @@
-var turf = require('@turf/turf');
-var fs = require('fs');
-
-// GEOJSON file of the street network
-var streets = fs.readFileSync('./input/full_dc.geojson');
-
-streetSegments = JSON.parse(streets);
