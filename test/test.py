7a01606da1ba5eac22c56935e1e73b36cada271e
Neil Chowdhury <neil.chowdhury@outlook.com>
2019-07-16 16:09:48 -0700

    modify test program to output accuracy

--- test/test.py
+++ test/test.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import math
 from intersection_proximity import compute_proximity
 
 predictor = compute_proximity
@@ -17,6 +18,21 @@ def test(row):
     print('predicted', a, b)
     print('actual', min_dist, middleness)
 
+    return pd.Series({
+        'absolute': math.fabs(min_dist - a) < 5,
+        'middleness': math.fabs(middleness - b) < 10
+    })
+
 
 ground_truth = pd.read_csv('ground_truth_intersection_proximity.csv')
-ground_truth.apply(test, axis=1)
+result = ground_truth.apply(test, axis=1)
+
+absolute_correct = sum(result.absolute)
+middleness_correct = sum(result.middleness)
+total_entries = len(result)
+
+print('-------------------------------')
+print('ABSOLUTE DISTANCE: ')
+print(f'\t {absolute_correct} / {total_entries} = {100*absolute_correct/total_entries:.2f}%')
+print('MIDDLENESS: ')
+print(f'\t {middleness_correct} / {total_entries} = {100*middleness_correct/total_entries:.2f}%')
